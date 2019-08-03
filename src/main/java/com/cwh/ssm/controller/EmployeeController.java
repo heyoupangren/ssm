02dde4396c9ddfa5b8eea685455ddedc76911ebb
heyoupangren <704208837@qq.com>
2019-08-03 22:03:27 +0800

    逆向工程，自动创建bean，mapper和mapper.xml文件

--- src/main/java/com/cwh/ssm/controller/EmployeeController.java
+++ src/main/java/com/cwh/ssm/controller/EmployeeController.java
@@ -6,23 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Map;
 
 /**
  * @author Cwh
- * CreateTime 2019/8/2 20:15
+ * CreateTime 2019/8/3 17:45
  */
 @Controller
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
-
     @RequestMapping("/emps")
-    public String getEmps(HttpServletResponse response,Map<String, Object> map){
-        response.setCharacterEncoding("utf-8");
+    public String getEmps(Map<String,Object> map){
         List<Employee> emps = employeeService.getEmps();
         map.put("emps",emps);
         return "list";
--- src/main/java/com/cwh/ssm/service/EmployeeService.java
+++ src/main/java/com/cwh/ssm/service/EmployeeService.java
@@ -1,16 +1,17 @@
 package com.cwh.ssm.service;
 
 import com.cwh.ssm.beans.Employee;
+import com.cwh.ssm.beans.EmployeeExample;
 import com.cwh.ssm.mapper.EmployeeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Cwh
- * CreateTime 2019/8/2 20:16
+ * CreateTime 2019/8/3 17:46
  */
 @Service
 public class EmployeeService {
@@ -18,10 +19,15 @@ public class EmployeeService {
     @Autowired
     private EmployeeMapper employeeMapper;
 
-
     public List<Employee> getEmps(){
-
-        List<Employee> emps = employeeMapper.getEmps();
-        return emps;
+        //查询的条件使用example进行封装
+        //example.createCriteria().andxxx();
+        EmployeeExample example =new EmployeeExample();
+        EmployeeExample.Criteria criteria = example.createCriteria();
+        criteria.andLastNameLike("%r%").andGenderEqualTo("1");
+        EmployeeExample.Criteria criteria1 = example.createCriteria();
+        criteria1.andEmailLike("%r%");
+        example.or(criteria1);
+        return employeeMapper.selectByExample(example);
     }
 }
